71f0c015ab41a1bd977a906be0f368eb0fe5df76
navuyi <figlusrafal@gmail.com>
2023-07-08 12:06:52 +0200

    restructuring

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import Counter from './components/Counter';
+import { store } from './src/redux/store';
+import Counter from './src/components/Counter';
 
 export default function App() {
  
